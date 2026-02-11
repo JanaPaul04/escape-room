4f0b79a5957f8ad98e2d7d0dc44ee20f69eaa7a7
JanaPaul04 <jana.c.paul@web.de>
2026-02-10 20:46:31 -0800

    Update game.js

--- game.js
+++ game.js
@@ -1,9 +1,42 @@
-function check() {
-  const a = document.getElementById("answer").value;
+// Fortschritt laden
+let progress = localStorage.getItem("progress") || 1;
+showRoom(progress);
 
-  if (a === "314") {
-    document.getElementById("result").innerText = "Tür offen!";
+// Raum anzeigen
+function showRoom(number) {
+  for (let i = 1; i <= 3; i++) {
+    document.getElementById("room" + i).style.display =
+      (i == number) ? "block" : "none";
+  }
+}
+
+// Raum 1 prüfen
+function checkRoom1() {
+  if (document.getElementById("answer1").value === "4") {
+    localStorage.setItem("progress", 2);
+    showRoom(2);
   } else {
-    document.getElementById("result").innerText = "Falsch";
+    alert("Falsch!");
+  }
+}
+
+// Raum 2 prüfen
+function checkRoom2() {
+  if (document.getElementById("answer2").value === "15") {
+    localStorage.setItem("progress", 3);
+    showRoom(3);
+  } else {
+    alert("Falsch!");
+  }
+}
+
+// Raum 3 prüfen
+function checkRoom3() {
+  if (document.getElementById("answer3").value === "8") {
+    alert("Du hast gewonnen!");
+    localStorage.setItem("progress", 1);
+    showRoom(1);
+  } else {
+    alert("Falsch!");
   }
 }
